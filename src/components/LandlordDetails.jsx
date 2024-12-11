391ab86da70bb04e54f6516fbe91a60138c9f91b
brewchetta <chett.tiller@gmail.com>
2024-12-11 11:58:18 -0500

    delete button

--- src/components/LandlordDetails.jsx
+++ src/components/LandlordDetails.jsx
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 function LandlordDetails() {
 
     const params = useParams()
+    const navigate = useNavigate()
 
     const [landlord, setLandlord] = useState({})
     const [errorMessage, setErrorMessage] = useState(null)
+
+    async function handleDelete() {
+        const response = await fetch(`http://localhost:5555/landlords/${landlord.id}`, { method: 'DELETE' })
+
+        if (response.ok) {
+            navigate('/')
+        } else {
+            setErrorMessage("That didn't work something went wrong")
+        }
+    }
 
     async function getLandlord() {
         const response = await fetch(`http://localhost:5555/landlords/${params.id}`)
@@ -39,6 +50,9 @@ function LandlordDetails() {
     return (
         <div>
             <h1>{landlord.associated_llcs}</h1>
+
+            <button onClick={handleDelete}>Delete Landlord</button>
+
             { mappedViolations }
         </div>
     )
